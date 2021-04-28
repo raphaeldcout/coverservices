9f802e6e4189e476f7613f175b3fbcc8e2cbaacc
Raphael Douglas <rafinha1132009@hotmail.com>
2021-04-28 02:42:32 -0300

    Primeiro commit #Raphael

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
 
     <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="/img/favicon.png">
+    <link rel="icon" type="image/png" href="/img/favicon2.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>{{ config('app.name', 'Coverservices') }}</title>
