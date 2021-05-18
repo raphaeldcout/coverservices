02c57a4a36e3149d2703c54616bb3c7cedb45e17
AndrLu <andreluizgoncalves@outlook.com.br>
2021-05-17 21:52:21 -0300

    Arquivo JS

---
+++ public/js/chamados/chamado.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function () {
+ $('#categoria').change(function (e) {
+     var _token = $('input[name="_token"]').val();
+     var categoria = $(this).val()
+     var url = '/search/select/categoria'
+     $.ajax({
+         type: "POST",
+         url: url,
+         data: {
+             _token: _token,
+             categoria: categoria
+         },
+         beforeSend: function() {
+
+         },
+         complete: function(data) {
+
+         },
+         success: function(data) {
+            data = JSON.parse(JSON.stringify(data));
+            problema = data.problemas;
+            setor    = data.setor;
+            $('#problema option').each(function(){
+                if($(this).val() != '-1'){
+                    this.remove();
+                }
+            })
+            problema.forEach((element,i)=>{
+                console.log(i)
+                if(i == 0){
+                    option = $('<option selected value="' + element.id + '">' + element.name + '</option>');
+                }else{
+                    option = $('<option value="' + element.id + '">' + element.name + '</option>');
+                }
+                
+                
+                $('#problema').append(option);
+
+            })  
+            
+            $('#setor option').each(function(){
+                if($(this).val() != '-1'){
+                    this.remove();
+                }
+            })
+            setor.forEach((element,i)=>{
+                if(i == 0){
+                    option = $('<option selected value="' + element.id + '">' + element.name + '</option>');
+                }else{
+                    option = $('<option value="' + element.id + '">' + element.name + '</option>');
+                }
+                
+                $('#setor').append(option);
+
+            })  
+
+         },
+         error: function(data) {
+
+         }
+     });
+ })
+});
