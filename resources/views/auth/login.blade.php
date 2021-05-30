fc7a5fcadf5bc3dbbedd769ec561c09d4d503d20
Raphael Douglas <rafinha1132009@hotmail.com>
2021-05-30 13:37:20 -0300

    #att para o servidor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,7 @@
                     <i class="material-icons">email</i>
                   </span>
                 </div>
-                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
+                <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
               </div>
               @if ($errors->has('email'))
                 <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
